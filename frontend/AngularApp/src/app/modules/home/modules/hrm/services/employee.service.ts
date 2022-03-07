7a1d955615ac45ad0e79331aa0a250cb58e68af6
Giahung <giahung127@gmail.com>
2022-03-07 22:55:04 +0700

    Test

--- frontend/AngularApp/src/app/modules/home/modules/hrm/services/employee.service.ts
+++ frontend/AngularApp/src/app/modules/home/modules/hrm/services/employee.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
+  headers = new HttpHeaders();
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
+
+  getAllEmployee (){
+    return this.http.get('', { headers: this.headers })
+  }
 }
