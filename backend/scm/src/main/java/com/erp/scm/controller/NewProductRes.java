c96ea810c6bfd6d44a7cb9e7f95aca9cc7673dff
Nguyen Minh Tri <minhtri.mountain@gmail.com>
2022-03-21 21:02:26 +0700

    Update Feature

---
+++ backend/scm/src/main/java/com/erp/scm/controller/NewProductRes.java
@@ -0,0 +1,13 @@
+package com.erp.scm.controller;
+
+public class NewProductRes {
+    public String code;
+    public String message;
+    public String data;
+
+    public NewProductRes(String code, String message, String data){
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+}
