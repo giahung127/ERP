97ef27e5d26d7baaead067911dcd63fcd9eb209b
minhtringuyen <minhtri.mountain@gmail.com>
2022-04-28 23:18:55 +0700

    Fix bug shipment
    Correct updateById UpdateShipmentReq

--- backend/scm/src/main/java/com/erp/scm/controller/request/UpdateShipmentReq.java
+++ backend/scm/src/main/java/com/erp/scm/controller/request/UpdateShipmentReq.java
@@ -1,10 +1,12 @@
 package com.erp.scm.controller.request;
 
 
+import com.erp.scm.controller.status.ShipmentStatus;
+
 public class UpdateShipmentReq {
     public String id;
-    public String order_id;
     public String receiver_name;
     public String contact_number;
     public String contact_address;
+    public ShipmentStatus shipment_status;
 }
