646a966a6c7296d9d0d4b817fc0de96ee92a68ad
minhtringuyen <minhtri.mountain@gmail.com>
2022-04-30 15:45:00 +0700

    Update Invoice
    New invoice take value from Oder directly, not from items
    getAll, getById available

--- backend/sale/src/main/java/com/erp/sale/controller/InvoiceController.java
+++ backend/sale/src/main/java/com/erp/sale/controller/InvoiceController.java
@@ -2,11 +2,17 @@ package com.erp.sale.controller;
 
 
 import com.erp.sale.controller.request.NewInvoiceReq;
+import com.erp.sale.controller.response.InvoiceWithItem;
+import com.erp.sale.controller.response.InvoiceWithItemRes;
+import com.erp.sale.controller.response.InvoicesWithItemRes;
 import com.erp.sale.controller.response.NormalRes;
+import com.erp.sale.entity.Invoice;
 import com.erp.sale.service.InvoiceService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -19,5 +25,15 @@ public class InvoiceController {
     @PostMapping("/newInvoice")
     public NormalRes newInvoice(@RequestBody NewInvoiceReq newInvoiceReq){
         return invoiceService.newInvoice(newInvoiceReq);
+    }
+
+    @GetMapping("getAll")
+    public InvoicesWithItemRes getAll(){
+        return invoiceService.getAll();
+    }
+
+    @GetMapping("/getById/{id}")
+    public InvoiceWithItemRes getById(@PathVariable String id){
+        return invoiceService.getById(id);
     }
 }
